73d978239977f65a5a9ebc42f11810c187b61279
lorenzochicco <lorenzo.chicco@studenti.unitn.it>
2024-10-12 17:11:33 +0200

    Updated docker compose file (name as well in order to use docker compose up command automatically. Added requirements txt for every data source (once the scripts are working we will remove the useless libraries), added fake main.py file to run containers without problems

---
+++ data/data_source/trustpilot/main.py
@@ -0,0 +1,3 @@
+# fake file just to run the docker compose correctly
+# every consumer will automatically start the main.py so we need a placeholder
+print("Trustpilot container started successfully")

7151bea4637934d132cdbadc44dab4b22300a00f
lorenzochicco <lorenzo.chicco@studenti.unitn.it>
2024-10-17 10:26:10 +0200

    Slightly improved the logic, added documentation

--- consumers/mongodb/main.py
+++ consumers/mongodb/main.py
@@ -3,10 +3,22 @@ from confluent_kafka import Consumer
 import json
 from datetime import datetime
 
-# Global variables for MongoDB collection
-collection = None
-
 def msg_process(msg, mongodb_collection):
+    """
+    Processes a single message obtained from the broker. 
+
+    Assumes that the message is a binary encoded json. It decodes it from binary to json and from json to dict.
+    We edit publishedAt field into a friendlier format for MongoDB, we may improve it later.
+    MongoDB wants a dictionary in order to use insert_one.
+    
+    Args:
+        msg (bytes): A binary-encoded message containing a JSON string.
+        mongodb_collection: A MongoDB collection.
+
+    Returns:
+        None
+    """
+
     # Decode the message value (assuming it is in JSON format)
     message_value = msg.value().decode('utf-8')
 
@@ -28,7 +40,40 @@ def msg_process(msg, mongodb_collection):
     # Store the data in MongoDB
     mongodb_collection.insert_one(data)  # Insert the document into the collection
 
-def basic_consume_loop(yt_consumer, mongodb_collection):
+def mongodb_setup():
+    """
+    Creates a MongoDB client, database and collection.
+
+    Args:
+        None
+
+    Returns:
+        MongoDB collection
+    """
+    # 1. Connect to MongoDB
+    # 2. Create a db called "mongoconsumer"
+    # 3. Create a collection inside the db "mongoconsumer"
+    # Connect to MongoDB running in Docker
+    client = get_mongo_connection()    
+    # Create or connect to a database
+    db = client.mongoconsumer  # mongoconsumer is the name of the db
+    # Create a collection (table equivalent in MongoDB).
+    mongodb_collection = db.messages
+    return mongodb_collection
+
+def basic_consume_loop(yt_consumer):
+    """
+    Starts the consume endless loop. Sets the MongoDB collection up and the Consumer starts polling the broker.
+    Each message gets checked and depending on the content it is either ignored, reported as an error (more resilient error 
+    handling will be implemented eventually) or processed calling the msg_process function.
+
+    args:
+        yt_consumer: confluent-kafka.Consumer object
+
+    returns:
+        None
+    """
+    mongodb_collection = mongodb_setup()
     state = True
     # dict_test = {"key" : 9}
     # mongodb_collection.insert_one(dict_test)
@@ -43,6 +88,15 @@ def basic_consume_loop(yt_consumer, mongodb_collection):
             msg_process(msg, mongodb_collection)
 
 def get_mongo_connection():
+    """
+    Establishes a MongoDB connection
+
+    args:
+        None
+
+    returns:
+        MongoClient object
+    """
     try:
         # Create a MongoDB client
         client = MongoClient('mongodb://mongo:27017/')
@@ -59,20 +113,17 @@ def get_mongo_connection():
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def consumer_setup():
+    """
+    Sets a confluent-kafka.Consumer up and subscribes to a hard coded list of topics, it will be later improved. 
 
-def main():
-    # 1. Connect to MongoDB
-    # 2. Create a db called "mongoconsumer"
-    # 3. Create a collection inside the db "mongoconsumer"
-    # 4. Insert files into the collection eventually
-    # Connect to MongoDB running in Docker
-    client = get_mongo_connection()    
-    # Create or connect to a database
-    db = client.mongoconsumer  # mongoconsumer is the name of the db
-    # Create a collection (table equivalent in MongoDB).
-    mongodb_collection = db.messages
-    
-    
+    args:
+        None
+
+    returns:
+        confluent-kafka.Consumer object
+    """
+
     # Setting up a Kafka Consumer. Config is standard and has to be custom made.
     consumer = Consumer({
         'bootstrap.servers': 'kafka:9092',  # Kafka broker
@@ -87,12 +138,21 @@ def main():
         # Subscribe to multiple topics
         consumer.subscribe(topics)
         print(f"Subscribed to topics: {topics}")
+        return consumer
 
     except Exception as e:
         print(f"Failed to subscribe: {e}")
 
+def main():
+    """
+    Coordinates the other functions to setup a Kafka Consumer that polls infinitely the broker and a mongodb collection which writes
+    the messages received. 
+    """
+
+    # Setup the consumer
+    consumer = consumer_setup()
     # Start consuming messages
-    basic_consume_loop(consumer, mongodb_collection)
+    basic_consume_loop(consumer)
 
 if __name__ == "__main__":
     main()

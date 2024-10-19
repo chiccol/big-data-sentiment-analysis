f349548e5803782e3c9e3b538ef45399ddb46d16
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-10-19 19:59:51 +0200

    Uploading fully functioning youtube scraper. Still needs to be tested as kafka producer.

--- data/data_source/youtube/main.py
+++ data/data_source/youtube/main.py
@@ -1,10 +1,11 @@
 from time import sleep
 import json
 from datetime import datetime
-from kafka_producer import KafkaProducer
 from dotenv import load_dotenv
+from kafka_producer import KafkaProducer
 import googleapiclient.discovery
 import os
+from youtube import search_videos, getcomments_video
 
 if __name__ == "__main__":
     
@@ -32,32 +33,53 @@ if __name__ == "__main__":
     print(f"Kafka producer {client_id} connected to {bootstrap_servers} for {source}")
 
     # Load companies and dates of the last scraping
-    companies_from_date_path = "urls-youtube.json"
-    with open(companies_from_date_path, 'r') as file:
-        companies_date = json.load(file)
-    print(f"Companies and dates of the last scraping loaded from {companies_from_date_path}")
-    for company in companies_date.keys():
-        print(f"Company: {company}, Last scraping: {companies_date[company]}")
+    companies_videos_path = "youtube_companies_videos.json"
+    with open(companies_videos_path, 'r') as file:
+        companies_videos = json.load(file)
+    print(f"Companies and videos of the last scraping loaded from {companies_videos_path}")
+    for company in companies_videos.keys():
+        print(f"Company: {company}, Last scraping: {companies_videos[company]}")
 
     date_format = "%Y-%m-%dT%H:%M:%SZ"
-
+    
     while True:
-        for company in companies_date.keys():
+        
+        for company in companies_videos.keys():
             
-            try:
-                from_date = datetime.strptime(companies_date[company], date_format)
-            except:
-                raise AssertionError(f"The date '{companies_date[company]}' does NOT match the format '{date_format}'")
+            print(f"Searching for new videos for {company}")
+            new_videos, companies_videos = search_videos(query = companies_videos[company]["query"],
+                                                         publishedAfter = companies_videos[company]["search_from_date"],
+                                                         youtube_scraper = youtube_scraper,
+                                                         company = company,
+                                                         max_videos = companies_videos[company]["max_videos"],
+                                                         relevanceLanguage = companies_videos[company]["relevance_language"],
+                                                         min_duration = companies_videos[company]["min_duration"],
+                                                         min_comment = companies_videos[company]["min_comment"],
+                                                         min_view = companies_videos[company]["min_view"])
             
-            
-            
-            # Update the date of the last scraping
-            companies_date[company] = datetime.now().strftime(date_format)
-            with open(companies_from_date_path, 'w') as file:
-                json.dump(companies_date, file)
+            for video in companies_videos[company]["videos"]:
+                if companies_videos[company]["videos"][video] not in ["too_short", "currently_irrelevant"] and\
+                    (companies_videos[company]["videos"][video] in new_videos or \
+                        companies_videos[company]["videos"][video].get("next_page_token", None) != None):
+                    print(companies_videos[company]["videos"][video])
 
-        sleep(30)  
+                    next_page_token = getcomments_video(video = video,
+                                                        youtube_scraper = youtube_scraper,
+                                                        company = company,
+                                                        producer = producer,
+                                                        max_num_comments = companies_videos[company]["max_num_comments_per_scraping"],
+                                                        next_page_token = companies_videos[company]["videos"][video]["next_page_token"],
+                                                        from_date = companies_videos[company]["videos"][video]["date_last_scrape"])
+                    if not next_page_token:
+                        # Update the date of the last scraping because all comments have been collected
+                        print(f"All comments have been collected for video {video}. Updating the date of the last scraping.")
+                        companies_videos[company]["videos"][video]["date_last_scrape"] = datetime.now().strftime(date_format)
+                    companies_videos[company]["videos"][video]["next_page_token"] = next_page_token
+                    with open(companies_videos_path, 'w') as file:
+                        json.dump(companies_videos, file, indent=4)
 
-        # Update dates of the last scraping
-        with open(companies_from_date_path, 'r') as file:
-            companies_date = json.load(file)
+        print("Sleeping for 5 minutes...")
+        sleep(300)  
+        print("Waking up!")
+        with open(companies_videos_path, 'r') as file:
+            companies_videos = json.load(file)

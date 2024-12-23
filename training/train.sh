2c99410d72bcc9c26e53259c7aced49b55a51336
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-12-23 18:49:03 +0100

    First draft to train model

---
+++ training/train.sh
@@ -0,0 +1,9 @@
+# trustpilot data
+gdown https://drive.google.com/uc?id=1wPJC00XvvTJ0wqQry0EmDG0hRmy7-pcd
+gdown https://drive.google.com/uc?id=1oXOdq8x4NuqAfbaR2ak8n4atclAlHxdz
+
+# yotube data
+gdown https://drive.google.com/uc?id=1ij9G4y2MqRuP1-s0KEcBSwGtimWz6uyf
+gdown https://drive.google.com/uc?id=13OCC_WsLB2Vwt3pz2nm_hEFjNszfLV15
+
+python3 main.py

d8b7097bc1c6b651ea7682a42b79ad2fcb949e8e
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-12-24 11:51:40 +0100

    Added training and validation utils to fine-tune. The checkpoints are not currently saved and no proper tracking is set up yet.

---
+++ training/main.py
@@ -0,0 +1,57 @@
+from trainer import get_model, run_epoch
+from utils import get_dataset, print_epoch_metrics
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from torch.nn import CrossEntropyLoss
+import torch 
+import pyaml
+
+def main():
+
+  # Load configuration
+  with open("config.yaml", "r") as file:
+    config = pyaml.load(file)
+
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  model, tokenizer = get_model(config["model_params"]["path"])
+  model.to(device)
+  
+  train_dataset, test_dataset = get_dataset(yt_train_path = config["data"]["yt_train_path"], 
+                                            yt_test_path = config["data"]["yt_test_path"], 
+                                            tp_train_path = config["data"]["tp_train_path"], 
+                                            tp_test_path = config["data"]["tp_test_path"], 
+                                            tokenizer = tokenizer)
+  
+  train_loader = DataLoader(train_dataset, batch_size=config["model_params"]["batch_size"], shuffle=True)
+  test_loader = DataLoader(test_dataset, batch_size=config["model_params"]["batch_size"])
+
+  # Optimizer and Loss
+  optimizer = AdamW(model.parameters(), lr=config["model_params"]["lr"])
+  loss_fn = CrossEntropyLoss()
+
+  epochs = config["model_params"]["epochs"]
+  for epoch in range(epochs):
+    # Run training
+    train_results = run_epoch(
+        model,
+        train_loader,
+        loss_fn,
+        optimizer=optimizer,
+        device=device,
+        desc=f"Epoch {epoch + 1}/{epochs} - Training",
+        label_names=["Positive", "Negative", "Neutral"]
+    )
+
+    # Run validation
+    val_results = run_epoch(
+        model,
+        test_loader,
+        loss_fn,
+        optimizer=None,
+        device=device,
+        desc=f"Epoch {epoch + 1}/{epochs} - Validating",
+        label_names=["Positive", "Negative", "Neutral"]
+    )
+
+    # Print results
+    print_epoch_metrics(epoch + 1, epochs, train_results, val_results)

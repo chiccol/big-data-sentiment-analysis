ed5604ce9e5f2f6c621608dab0b46fb9acb59de8
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-12-24 11:51:40 +0100

    Added training and validation utils to fine-tune. The checkpoints are not currently saved and no proper tracking is set up yet.

--- training/trainer.py
+++ training/trainer.py
@@ -1,147 +1,63 @@
 import torch
-from torch.utils.data import DataLoader, Dataset
-from torch.optim import AdamW
-from torch.nn import CrossEntropyLoss
 import torch.nn as nn
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
-from sklearn.model_selection import train_test_split
 
-import json
-import pandas as pd
+from utils import compute_metrics, compute_label_wise_metrics, compute_source_wise_metrics
 from tqdm import tqdm
 
-# Hyperparameters
-batch_size = 64
-learning_rate = 1e-3
-epochs = 10
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def run_epoch(
+    model, 
+    dataloader, 
+    loss_fn, 
+    optimizer=None, 
+    device="cuda", 
+    desc="Training", 
+    label_names=None
+):
+    is_train = optimizer is not None
+    model.train() if is_train else model.eval()
 
-hf_model = "distilbert-base-uncased-finetuned-sst-2-english"
-tokenizer = DistilBertTokenizer.from_pretrained(hf_model)
-model = DistilBertForSequenceClassification.from_pretrained(hf_model)
-model.classifier = nn.Linear(768, 3, bias=True)
-model = model.to(device)
-
-# Freeze all layers except the classification head
-for param in model.base_model.parameters():
-    param.requires_grad = False
-
-# Custom Dataset Class
-class SentimentDataset(Dataset):
-    def __init__(self, texts, labels, tokenizer, max_len=512):
-        self.texts = texts
-        self.labels = labels
-        self.tokenizer = tokenizer
-        self.max_len = max_len
-
-    def __len__(self):
-        return len(self.texts)
-
-    def __getitem__(self, idx):
-        text = self.texts[idx]
-        label = self.labels[idx]
-        encoding = self.tokenizer(
-            text,
-            truncation=True,
-            padding="max_length",
-            max_length=self.max_len,
-            return_tensors="pt"
-        )
-        return {
-            "input_ids": encoding["input_ids"].squeeze(0),
-            "attention_mask": encoding["attention_mask"].squeeze(0),
-            "label": torch.tensor(label, dtype=torch.long)
-        }
-    
-train_path = r"/content/training_dataset.json"
-test_path = r"/content/diff_companies_test_dataset.json"
-
-train_data = pd.read_json(train_path)
-test_data = pd.read_json(test_path)
-
-def categorical_encoding(data: pd.Series):
-  if data == "negative":
-    return 0
-  elif data == "neutral":
-    return 1
-  elif data == "positive":
-    return 2
-
-train_data["sentiment_encoded"] = train_data.sentiment.apply(categorical_encoding)
-test_data["sentiment_encoded"] = test_data.sentiment.apply(categorical_encoding)
-
-# Prepare Dataset
-train_texts = train_data.text
-train_labels = train_data.sentiment_encoded
-
-test_texts = test_data.text
-test_labels = test_data.sentiment_encoded
-
-train_dataset = SentimentDataset(train_texts, train_labels, tokenizer)
-val_dataset = SentimentDataset(test_texts, test_labels, tokenizer)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size)
-
-# Optimizer and Loss
-optimizer = AdamW(model.parameters(), lr=learning_rate)
-loss_fn = CrossEntropyLoss()
-
-# Training Loop
-for epoch in range(epochs):
-    model.train()
-    train_loss = 0.0
+    total_loss = 0.0
     correct = 0
-    print(f"Epoch {epoch + 1}/{epochs}")
-    for batch in tqdm(train_loader, desc="Training", leave=False, ncols=80):
-        optimizer.zero_grad()
+    y_true, y_pred, sources = [], [], []
 
-        # Move data to device
+    loop = tqdm(dataloader, desc=desc, leave=False, ncols=80, unit="batch")
+    for batch in loop:
         input_ids = batch["input_ids"].to(device)
         attention_mask = batch["attention_mask"].to(device)
         labels = batch["label"].to(device)
+        source = batch["source"]
 
-        # Forward pass
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        logits = outputs.logits
-        loss = loss_fn(logits, labels)
-
-        # Backward pass
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        preds = torch.argmax(logits, dim=1)
-        correct += (preds == labels).sum().item()
-
-        # Print loss for this batch
-        tqdm.write(f"Batch Loss: {loss.item():.4f}", end="\r")
-
-    avg_train_loss = train_loss / len(train_loader)
-    avg_accuracy = correct / len(train_loader.dataset)  # Correctly adjusted
-    print(f"Epoch {epoch + 1}/{epochs}, Training Loss: {avg_train_loss:.4f}, Training Accuracy: {avg_accuracy:.4f}")
-
-    # Validation Loop
-    model.eval()
-    val_loss = 0.0
-    correct = 0
-    total = 0
-    for batch in tqdm(val_loader, desc="Validating", leave=False, ncols=80):
-        with torch.no_grad():
-            input_ids = batch["input_ids"].to(device)
-            attention_mask = batch["attention_mask"].to(device)
-            labels = batch["label"].to(device)
-
+        with torch.set_grad_enabled(is_train):
             outputs = model(input_ids=input_ids, attention_mask=attention_mask)
             logits = outputs.logits
             loss = loss_fn(logits, labels)
+            if is_train:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            val_loss += loss.item()
+        total_loss += loss.item()
+        preds = torch.argmax(logits, dim=1)
+        correct += (preds == labels).sum().item()
 
-            # Calculate accuracy
-            preds = torch.argmax(logits, dim=1)
-            correct += (preds == labels).sum().item()
+        y_true.extend(labels.cpu().tolist())
+        y_pred.extend(preds.cpu().tolist())
+        sources.extend(source)
 
-    avg_val_loss = val_loss / len(val_loader)
-    accuracy = correct / len(val_loader.dataset)  # Correctly adjusted
-    print(f"Validation Loss: {avg_val_loss:.4f}, Accuracy: {accuracy:.4f}")
+        loop.set_postfix({"Batch Loss": f"{loss.item():.4f}"})
+
+    avg_loss = total_loss / len(dataloader)
+    metrics = compute_metrics(y_true, y_pred)
+    label_metrics = compute_label_wise_metrics(y_true, y_pred, label_names=label_names)
+    source_metrics = compute_source_wise_metrics(y_true, y_pred, sources)
+    return avg_loss, metrics, label_metrics, source_metrics
+
+def get_model(path):
+  tokenizer = DistilBertTokenizer.from_pretrained(path)
+  model = DistilBertForSequenceClassification.from_pretrained(path)
+  model.classifier = nn.Linear(768, 3, bias=True)
+  # Freeze all layers except the classification head
+  for param in model.base_model.parameters():
+      param.requires_grad = False
+  return model, tokenizer
---
+++ training/utils.py
@@ -0,0 +1,140 @@
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import numpy as np 
+import pandas as pd
+from torch.utils.data import Dataset
+import torch 
+
+from collections import defaultdict
+
+# Custom Dataset Class
+class SentimentDataset(Dataset):
+    def __init__(self, texts, labels, source, tokenizer, max_len=512):
+        self.texts = texts
+        self.labels = labels
+        self.source = source
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        source = self.source[idx]
+        encoding = self.tokenizer(
+            text,
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_len,
+            return_tensors="pt"
+        )
+        return {
+            "input_ids": encoding["input_ids"].squeeze(0),
+            "attention_mask": encoding["attention_mask"].squeeze(0),
+            "label": torch.tensor(label, dtype=torch.long),
+            "source" : source
+        }
+
+def get_dataset(yt_train_path, yt_test_path, tp_train_path, tp_test_path, tokenizer):
+
+  tp_train_data = pd.read_json(tp_train_path)
+  tp_train_data["source"] = "trustpilot"
+  tp_test_data = pd.read_json(tp_test_path)
+  tp_test_data["source"] = "trustpilot"
+
+  yt_test_data = pd.read_json(yt_test_path)
+  yt_test_data["source"] = "youtube"
+  yt_train_data = pd.read_json(yt_train_path)
+  yt_train_data["source"] = "youtube"
+
+  train_data = pd.concat([tp_train_data.loc[:,["text","sentiment", "source"]],
+                        yt_train_data.loc[:,["text","sentiment", "source"]]])
+  test_data = pd.concat([tp_test_data.loc[:,["text","sentiment", "source"]],
+                        yt_test_data.loc[:,["text","sentiment", "source"]]])
+  
+  sentiment_mapping = {"negative":0, "neutral":1, "positive":2}
+  train_data["sentiment_encoded"] = train_data.sentiment.map(sentiment_mapping)
+  test_data["sentiment_encoded"] = test_data.sentiment.map(sentiment_mapping)
+
+  # Prepare Dataset
+  train_texts = train_data.text.to_list()
+  train_labels = train_data.sentiment_encoded.to_list()
+  train_source = train_data.source.to_list()
+
+  test_texts = test_data.text.to_list()
+  test_labels = test_data.sentiment_encoded.to_list()
+  test_source = test_data.source.to_list()
+
+  train_dataset = SentimentDataset(train_texts, train_labels, train_source, tokenizer)
+  test_dataset = SentimentDataset(test_texts, test_labels, test_source, tokenizer)
+
+  return train_dataset, test_dataset
+
+def compute_metrics(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average="macro", zero_division=0)
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+
+def compute_label_wise_metrics(y_true, y_pred, label_names):
+    precision, recall, f1, support = precision_recall_fscore_support(y_true, y_pred, average=None, zero_division=0)
+    
+    metrics = {}
+    for i, label in enumerate(label_names):
+        # Compute accuracy for this label
+        correct = np.sum((np.array(y_true) == i) & (np.array(y_pred) == i))  # Correct predictions for this label
+        total = support[i]  # Total occurrences of the label in the true labels
+        label_accuracy = correct / total if total > 0 else 0  # Accuracy for the label
+
+        # Store the label-wise metrics
+        metrics[label] = {
+            "accuracy": label_accuracy,
+            "precision": precision[i],
+            "recall": recall[i],
+            "f1": f1[i],
+        }
+    
+    return metrics
+
+def compute_source_wise_metrics(y_true, y_pred, sources):
+    source_metrics = defaultdict(lambda: defaultdict(float))
+    source_to_labels = defaultdict(list)
+    source_to_preds = defaultdict(list)
+
+    for src, label, pred in zip(sources, y_true, y_pred):
+        source_to_labels[src].append(label)
+        source_to_preds[src].append(pred)
+
+    for src in source_to_labels:
+        metrics = compute_metrics(source_to_labels[src], source_to_preds[src])
+        source_metrics[src] = metrics
+
+    return dict(source_metrics)
+
+def print_epoch_metrics(epoch, epochs, train_results, val_results):
+    print(f"\nEpoch {epoch}/{epochs}")
+
+    train_loss, train_accuracy, train_global, train_labels, train_sources = train_results
+    print(f"\nTraining Metrics:")
+    print(f"  Loss: {train_loss:.4f}")
+    train_global = {key: f"{value:.4f}" for key, value in train_global.items()}
+    print(f"  Global: {train_global}")
+    print(f"  Label-wise:")
+    for label, metrics in train_labels.items():
+        print(f"    {label}: {metrics}")
+    print(f"  Source-wise:")
+    for source, metrics in train_sources.items():
+        print(f"    {source}: {metrics}")
+
+    val_loss, val_global, val_labels, val_sources = val_results
+    print(f"\nValidation Metrics:")
+    print(f"  Loss: {val_loss:.4f}")
+    val_global = {key: f"{value:.4f}" for key, value in val_global.items()}
+    print(f"  Global: {val_global}")
+    print(f"  Label-wise:")
+    for label, metrics in val_labels.items():
+        print(f"    {label}: {metrics:.4f}")
+    print(f"  Source-wise:")
+    for source, metrics in val_sources.items():
+        print(f"    {source}: {metrics:.4f}")
+    print("-" * 50)

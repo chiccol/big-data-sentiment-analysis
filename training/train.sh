e082c2ca2147d5702212279dd5f884412f9480f6
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-12-24 19:55:31 +0100

    Adjusted train and config

--- training/train.sh
+++ training/train.sh
@@ -1,33 +1,33 @@
-tp_test="training/diff_companies_test_dataset.json"
-tp_train="training/training_dataset.json"
-yt_test="training/test_dataset.json"
-yt_train="training/train_dataset_balanced.json"
+tp_test="training/tp_diff_companies_test_dataset.json"
+tp_train="training/tp_training_dataset.json"
+yt_test="training/yt_test_dataset.json"
+yt_train="training/yt_train_dataset_balanced.json"
 
 # Check if the files already exist
-if [ ! -f "$tp_test" ]; then
-    echo "Downloading Trustpilot test..."
-    gdown https://drive.google.com/uc?id=1wPJC00XvvTJ0wqQry0EmDG0hRmy7-pcd --output training/diff_companies_test_dataset.json
+if [ ! -f "$tp_test" ]; then 
+    echo "Downloading Trustpilot test..." 
+    gdown https://drive.google.com/uc?id=1EguvQcsht2zbHhxt-XqXWZ81fgVuHVxu --output training/diff_companies_test_dataset.json
 else
     echo "Trustpilot test already exists."
 fi
 
 if [ ! -f "$tp_train" ]; then
-    echo "Downloading Trustpilot train..."
-    gdown https://drive.google.com/uc?id=1oXOdq8x4NuqAfbaR2ak8n4atclAlHxdz --output training/training_dataset.json
+    echo "Downloading Trustpilot train..." 
+    gdown https://drive.google.com/uc?id=14Z2nbEyqOTkU2FD4EkWxUzUTaWs_CuBQ --output training/tp_training_dataset.json
 else
     echo "Trustpilot train already exists."
 fi
 
-if [ ! -f "$yt_test" ]; then
+if [ ! -f "$yt_test" ]; then 
     echo "Downloading YouTube test..."
-    gdown https://drive.google.com/uc?id=1ij9G4y2MqRuP1-s0KEcBSwGtimWz6uyf --output training/test_dataset.json
+    gdown https://drive.google.com/uc?id=1WqV01XrxhEwERehjdmBXvJTJQjGWLPMX --output training/yt_test_dataset.json
 else
     echo "YouTube test already exists."
 fi
 
-if [ ! -f "$yt_train" ]; then
+if [ ! -f "$yt_train" ]; then 
     echo "Downloading YouTube train..."
-    gdown https://drive.google.com/uc?id=13OCC_WsLB2Vwt3pz2nm_hEFjNszfLV15 --output training/train_dataset_balanced.json
+    gdown https://drive.google.com/uc?id=12EwKMCeEdljQG4SQ0of4FqZ1SnobIdbk --output training/yt_train_dataset_balanced.json
 else
     echo "YouTube train already exists."
 fi

4ba366e99c7d49533638c25efa2e87ed4d0faec3
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-12-28 17:33:42 +0100

    changed gdown link data

--- training/train.sh
+++ training/train.sh
@@ -1,33 +1,33 @@
 tp_test="training/tp_diff_companies_test_dataset.json"
-tp_train="training/tp_training_dataset.json"
+tp_train="training/tp_train_dataset.json"
 yt_test="training/yt_test_dataset.json"
 yt_train="training/yt_train_dataset_balanced.json"
 
 # Check if the files already exist
 if [ ! -f "$tp_test" ]; then 
     echo "Downloading Trustpilot test..." 
-    gdown https://drive.google.com/uc?id=1EguvQcsht2zbHhxt-XqXWZ81fgVuHVxu --output training/tp_diff_companies_test_dataset.json
+    gdown https://drive.google.com/uc?id=1PeJnwpbcpeG1QTEwTHMaU8dIMz1I_BEG --output training/tp_diff_companies_test_dataset.json
 else
     echo "Trustpilot test already exists."
 fi
 
 if [ ! -f "$tp_train" ]; then
     echo "Downloading Trustpilot train..." 
-    gdown https://drive.google.com/uc?id=14Z2nbEyqOTkU2FD4EkWxUzUTaWs_CuBQ --output training/tp_training_dataset.json
+    gdown https://drive.google.com/uc?id=1z8vI1WSHba6Ag7OfzXfBEpPp8OtKrH3B --output training/tp_train_dataset.json
 else
     echo "Trustpilot train already exists."
 fi
 
 if [ ! -f "$yt_test" ]; then 
     echo "Downloading YouTube test..."
-    gdown https://drive.google.com/uc?id=1WqV01XrxhEwERehjdmBXvJTJQjGWLPMX --output training/yt_test_dataset.json
+    gdown https://drive.google.com/uc?id=17me6NiVPgogKvhDdiIfu-BC38QSsjx3b --output training/yt_test_dataset.json
 else
     echo "YouTube test already exists."
 fi
 
 if [ ! -f "$yt_train" ]; then 
     echo "Downloading YouTube train..."
-    gdown https://drive.google.com/uc?id=12EwKMCeEdljQG4SQ0of4FqZ1SnobIdbk --output training/yt_train_dataset_balanced.json
+    gdown https://drive.google.com/uc?id=1ZkrkmXUJE67ieP6OlwCz8Vx9u6sKURBc --output training/yt_train_dataset_balanced.json
 else
     echo "YouTube train already exists."
 fi
